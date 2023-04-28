5a116ae42830c17d095fcc37acdfa3a6413c8454
Medynskyy Ostap <medinsky_os@groupbwt.com>
2023-04-28 17:37:59 +0300

    add urql and codegen to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,24 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { Client, Provider, cacheExchange, fetchExchange } from 'urql';
 
 import theme from '../theme';
 import { AppProps } from 'next/app';
 
+const client = new Client({
+  url: 'http://localhost:5000/graphql',
+  fetchOptions: {
+    credentials: 'include',
+  },
+  exchanges: [cacheExchange, fetchExchange],
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Provider value={client}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
   );
 }
 
