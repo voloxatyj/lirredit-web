c7f79367302dddeef4fea47036f3a2f1de623411
Medynskyy Ostap <medinsky_os@groupbwt.com>
2023-05-02 13:17:05 +0300

    Add global components

---
+++ src/styles/input.ts
@@ -0,0 +1,39 @@
+import type { ComponentStyleConfig } from '@chakra-ui/theme';
+
+export const Input: ComponentStyleConfig = {
+  baseStyle: {
+    field: {
+      fontSize: '10pt',
+      bg: 'gray.50',
+      _placeholder: {
+        color: 'gray.500',
+      },
+      _hover: {
+        bg: 'white',
+        border: '1px solid',
+        borderColor: 'blue.500',
+      },
+      _focus: {
+        outline: 'none',
+        border: '1px solid',
+        borderColor: 'blue.500',
+      },
+    },
+    addons: {
+      height: '30px',
+    },
+  },
+  sizes: {
+    md: {
+      field: {
+        height: "50px",
+        fontSize: '14pt',
+        borderRadius: '20px'
+      },
+    },
+  },
+  variants: {},
+  defaultProps: {
+    variant: null,
+  },
+};
