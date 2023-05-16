93f3cf4d4f6be96e057d9eba4bf964ed11950350
Medynskyy Ostap <medinsky_os@groupbwt.com>
2023-05-16 16:43:35 +0300

    Change rules

--- src/components/Layout/PageContentLayout.tsx
+++ src/components/Layout/PageContentLayout.tsx
@@ -4,22 +4,22 @@ import { Box } from '@chakra-ui/react';
 export type PageContentLayoutVariant = 'small' | 'regular';
 
 interface PageContentLayoutProps {
-  variant?: PageContentLayoutVariant;
-  children: JSX.Element;
+	variant?: PageContentLayoutVariant;
+	children: JSX.Element;
 }
 
 export const PageContentLayout: React.FC<PageContentLayoutProps> = ({
-  children,
-  variant = 'regular',
+	children,
+	variant = 'regular',
 }) => {
-  return (
-    <Box
-      mt={8}
-      mx='auto'
-      maxW={variant === 'regular' ? '800px' : '400px'}
-      w='100%'
-    >
-      {children}
-    </Box>
-  );
+	return (
+		<Box
+			mt={8}
+			mx='auto'
+			maxW={variant === 'regular' ? '800px' : '400px'}
+			w='100%'
+		>
+			{children}
+		</Box>
+	);
 };
