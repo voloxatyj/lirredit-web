a30c991d3ff6be2f82734e5dcdc5209d9cd8d2be
Medynskyy Ostap <medinsky_os@groupbwt.com>
2023-06-13 18:47:40 +0300

    Change start page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,28 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react';
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons';
+import React from 'react';
+import Image from 'next/image';
+import { PageContentLayout } from '../components/Layout/PageContentLayout';
+import logo from '../assets/icon.png';
+import { Heading } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 
-import { Hero } from '../components/Hero';
-import { Container } from '../components/Container';
-import { Main } from '../components/Main';
-import { CTA } from '../components/CTA';
-import { Footer } from '../components/Footer';
-
-const Index = () => (
-  <Container height='100vh'>
-    <Hero />
-    <Main>
-      <Text color='text'>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>TypeScript</Code>.
-      </Text>
-
-      <List spacing={3} my={0} color='text'>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color='green.500' />
-          <ChakraLink
-            isExternal
-            href='https://chakra-ui.com'
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color='green.500' />
-          <ChakraLink isExternal href='https://nextjs.org' flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
-
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-);
+const Index = () => {
+	const router = useRouter();
+	return (
+		<PageContentLayout>
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					marginTop: '40vh',
+					cursor: 'pointer',
+				}}
+				onClick={() => router.push('/login')}
+			>
+				<Image src={logo} alt='logo' width={50} height={50} />
+				<Heading fontSize={80}>{'LirrEditChat'}</Heading>
+			</div>
+		</PageContentLayout>
+	);
+};
 
 export default Index;
