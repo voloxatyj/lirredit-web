0f9598a0371f517767b1f5b2fb552d293d3e41b0
Medynskyy Ostap <medinsky_os@groupbwt.com>
2023-07-17 13:05:25 +0300

    add .env to config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ module.exports = {
 	images: {
 		domains: ['res.cloudinary.com'],
 	},
+	env: {
+		NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+		IMAGES_API: process.env.IMAGES_API,
+	},
 };
